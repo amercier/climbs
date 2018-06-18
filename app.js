172feb3bdf480cd94a07279a1fd19693cf60ac46
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-06-18 17:27:13 +0200

    Added router error handling

--- app.js
+++ app.js
@@ -5,23 +5,30 @@ const { green, magenta, red } = require('chalk');
 const { info, error } = require('./lib/log');
 const {
   requestLogMiddleware,
-  headersDebugMiddleware,
-  sessionDebugMiddleware,
-  userDebugMiddleware,
+  // headersDebugMiddleware,
+  // sessionDebugMiddleware,
+  // userDebugMiddleware,
 } = require('./lib/middleware');
+const { notFoundMiddleware, errorRenderer } = require('./lib/error');
+
+const config = {
+  public: 'public',
+};
 
 const home = () => render('home.pug', { title: 'Strava Climbs' });
-
 const routes = [
   get('/', home),
 ];
 
 module.exports = server(
+  config,
   requestLogMiddleware,
-  headersDebugMiddleware,
-  sessionDebugMiddleware,
-  userDebugMiddleware,
+  // headersDebugMiddleware,
+  // sessionDebugMiddleware,
+  // userDebugMiddleware,
   routes,
+  notFoundMiddleware,
+  errorRenderer,
 ).then(({ options }) => {
   const { port, env } = options;
   info(`Started server in ${green(env)} mode, listening on port ${magenta(port)}`);
